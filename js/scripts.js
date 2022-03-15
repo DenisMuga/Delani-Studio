b0b39c8a87268367834bb0fbec3b523164772c7d
DenisMuga <denismugah5@gmail.com>
2022-03-15 05:18:08 +0300

    chore: adding js scripts to improve page interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,114 @@
 $(document).ready(function(){
-    
-})
+    $("#development-image").click(function(){
+        $("#development-image").slideDown('1500').hide(1000);
+        $("#development").show('1500');
+    });
+    $("#development-image").click(function(){
+        $("#development-image").slideUp('1500');
+        $("#development-image").show('1500');
+    });
+});
+$(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });   
+
+  $(document).ready(function(){
+    $("#plain").mouseover(function(){
+      $("#overlay").show();
+    }).mouseout(function(){
+      $("#overlay").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#Ontario").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Prisms").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#uxui").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Interface").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#DBA").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#Branding").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#Proto").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
+  
+
+
+
+  
