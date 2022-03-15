fd58b2771224c94c77542ba4641b5bd0a3b49b60
DenisMuga <denismugah5@gmail.com>
2022-03-15 09:59:50 +0300

    chore: adding js scripts to manipulate the portfolio paragraphs

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#development-image").click(function(){
         $("#development-image").slideDown(1500).hide(1000);
         $("#development").show(1500);
+        $("#development>p").show(1500);
     });
     $("#development-image").click(function(){
         $("#development-image").slideUp(1500);
@@ -12,6 +13,7 @@ $(document).ready(function(){
     $("#design-image").click(function(){
       $("#design-image").slideDown(1500).hide(1000);
       $("#design").show(1500);
+      $("#design>p").show(1500);
     });
     $("#design-image").click(function(){
       $("#design image").slideUp(1500);
@@ -23,6 +25,7 @@ $(document).ready(function(){
     $("#product-image").click(function(){
       $("#product-image").slideDown(1500).hide(1000);
       $("#product").show(1500);
+      $("#product>p").show(1500);
     });
     $("#product-image").click(function(){
       $("#product-image").slideUp(1500);
